95aad4acef041d70ecce24dce79aa02dfbaefd3e
Oliver Zott <zott_oliver@web.de>
2022-05-06 18:04:42 +0200

    # Initial commit

---
+++ velosaurus-backend/Program.cs
@@ -0,0 +1,42 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://192.168.0.18:3000",
+                                              "http://localhost:3000",
+                                              "localhost:3000");
+                      });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapControllers();
+
+app.Run();
---
+++ velosaurus-backend/README.md
@@ -0,0 +1,10 @@
+﻿# Setup project
+- new asp.net api project
+- CORS added [Link](https://docs.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-6.0)
+
+
+# Next steps
+- Dummy controller
+	- MtbTour
+- MongoDB
+	- https://www.youtube.com/watch?v=exXavNOqaVo
