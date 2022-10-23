bc86f2fd9cebee401f8c6e640f1fcecaedcf1e09
Oliver Zott <zott_oliver@web.de>
2022-10-23 18:42:38 +0200

    test

--- velosaurus-backend/Program.cs
+++ velosaurus-backend/Program.cs
@@ -39,12 +39,8 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwaggerUI();
-}
-
 app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
