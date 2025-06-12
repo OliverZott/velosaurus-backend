5ddad579e04a71539eeeea26dc04dfeef91d63bb
Oliver Zott <zott_oliver@web.de>
2025-06-12 20:17:54 +0200

    refactor: exception middleware housekeeping

--- src/Velosaurus.Core/Middleware/ExceptionMiddleware/ExceptionMiddleware.cs
+++ src/Velosaurus.Core/Middleware/ExceptionMiddleware/ExceptionMiddleware.cs
@@ -3,40 +3,33 @@ using Microsoft.Extensions.Logging;
 
 namespace Velosaurus.Core.Middleware.ExceptionMiddleware;
 
-public class ExceptionMiddleware
+public class ExceptionMiddleware(
+    RequestDelegate requestDelegate,
+    ILogger<ExceptionMiddleware> logger,
+    IExceptionHandler exceptionHandler)
 {
-    private readonly RequestDelegate _requestDelegate;
-    private readonly ILogger<ExceptionMiddleware> _logger;
-    private readonly IExceptionHandler _exceptionHandler;
-
     // Default ExceptionHandler, if not registered in IoC container (program.cs), which it is :)
     public ExceptionMiddleware(RequestDelegate requestDelegate, ILogger<ExceptionMiddleware> logger) : this(requestDelegate, logger, new ExceptionHandler()) { }
-
-    public ExceptionMiddleware(RequestDelegate requestDelegate, ILogger<ExceptionMiddleware> logger, IExceptionHandler exceptionHandler)
-    {
-        _requestDelegate = requestDelegate;
-        _logger = logger;
-        _exceptionHandler = exceptionHandler;
-    }
 
     public async Task InvokeAsync(HttpContext context)
     {
         try
         {
             var request = context.Request;
-            // TODO remark structured logging
-            _logger.LogInformation("Handling request: {method} {path} from {ip}",
+
+            logger.LogInformation("Handling request: {method} {path} from {ip}",
                 context.Request.Method,
                 context.Request.Path,
                 context.Connection.RemoteIpAddress?.ToString());
-            await _requestDelegate(context);
+
+            await requestDelegate(context);
         }
         catch (Exception e)
         {
-            _logger.LogError(e, "LogError: Exception while processing {path}: '{message}'",
+            logger.LogError(e, "LogError: Exception while processing {path}: '{message}'",
                 context.Request.Path,
                 e.Message);
-            _ = _exceptionHandler.HandleExceptionAsync(context, e);
+            _ = exceptionHandler.HandleExceptionAsync(context, e);
         }
     }
 }
